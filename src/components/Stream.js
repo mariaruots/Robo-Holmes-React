1d7db6bcab792a98488076911962f481b92c9ed0
Max <maxkorpinen@gmail.com>
2019-12-17 14:48:54 +0200

    fixed the stream wrapper

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -4,13 +4,12 @@ import JSMpeg from '@cycjimmy/jsmpeg-player';
 export default class Stream extends Component {
 
     componentDidMount() {
-        new JSMpeg.VideoElement('#videoStream', 'ws://'+document.location.hostname+':8082/');
+        new JSMpeg.VideoElement('.videoWrapper', 'ws://'+document.location.hostname+':8082/');
     }
 
     render() {     
         return (
             <div className="videoWrapper">
-                <canvas id="videoStream"></canvas>
             </div>
         )
     }
